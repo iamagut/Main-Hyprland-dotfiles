cdf2e28650b7b6542eabd76d5b9b0466bb1682d0
iamagut <57557058+iamagut@users.noreply.github.com>
2025-12-06 04:15:56 +0700

    Delete .config/hypr/Scripts/keybinds_hints.sh

--- .config/hypr/Scripts/keybinds_hints.sh
+++
@@ -1,8 +0,0 @@
-# Path to your hyprland config
-CONFIG="$HOME/.config/hypr/config/keybindings.conf"
-
-# Grep lines starting with 'bind', clean them up, and show in rofi
-grep -E "^bind" "$CONFIG" | \
-    sed 's/bind = //g' | \
-    sed 's/exec, //g' | \
-    rofi -dmenu -p "Keybinds" -i -theme-str 'window {width: 800px;}'
