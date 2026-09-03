778353f91b2420f7aa9a7ebe1a11ef7fdb74767e
iamagut <57557058+iamagut@users.noreply.github.com>
2026-09-03 15:13:20 +0700

    Change monitor configuration from HDMI-A-1 to eDP-1

--- .config/hypr/config/monitor.lua
+++ .config/hypr/config/monitor.lua
@@ -2,14 +2,14 @@
 -- ║                           Monitor                             ║
 -- ╚═══════════════════════════════════════════════════════════════╝
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
- 
+
 hl.monitor({
-    output   = "HDMI-A-1",
-    mode     = "1920x1080@75.00",
-    position = "0x0",
-    scale    = 1,
+	output = "eDP-1",
+	mode = "1920x1080@60.00",
+	position = "0x0",
+	scale = 1.25,
 })
 
 for i = 1, 5 do
-    hl.workspace_rule({ workspace = tostring(i), monitor = "HDMI-A-1", persistent = true })
+	hl.workspace_rule({ workspace = tostring(i), monitor = "eDP-1", persistent = true })
 end
