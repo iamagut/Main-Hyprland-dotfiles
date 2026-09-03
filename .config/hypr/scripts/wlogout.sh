a940c36d5e5381d8d467f8ed78082183889a0b20
iamagut <57557058+iamagut@users.noreply.github.com>
2026-09-03 15:15:41 +0700

    Enhance wlogout script with quickshell check
    Add check for quickshell process and toggle logout menu if running.

--- .config/hypr/scripts/wlogout.sh
+++ .config/hypr/scripts/wlogout.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if pgrep -x "quickshell" > /dev/null; then
+    quickshell ipc call qsIpc toggleLogoutMenu
+    exit 0
+fi
+
 # Check if wlogout is already running
 if pgrep -x "wlogout" > /dev/null; then
     pkill -x "wlogout"
